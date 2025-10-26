9a85057cb4a1de15c1f39c47ad1761163793181b
benjqminn <benjqmin13@gmail.com>
2025-10-26 11:01:43 -0400

    Improve user detail and photo components
    Refactored UserDetail and UserPhotos components to handle prop changes and fetch data accordingly. Updated UserDetail to use React Router's Link for navigation and improved markup structure. Enhanced UserPhotos to display user photos and added styles for better formatting. Updated CSS for both components to improve layout and readability.

--- components/userDetail/userDetail.jsx
+++ components/userDetail/userDetail.jsx
@@ -1,47 +1,50 @@
 import React from 'react';
-import {
-  Typography
-} from '@mui/material';
+import { Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
 import './userDetail.css';
 import fetchModel from '../../lib/fetchModelData';
 
 /**
  * Define UserDetail, a React component of project #5
  */
+
 class UserDetail extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { user: null, error: null };
   }
 
-  componentDidMount() {
+  componentDidMount() { this.loadUser(); }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.match.params.userId !== this.props.match.params.userId) {
+      this.loadUser();
+    }
+  }
+
+  loadUser = () => {
     const { userId } = this.props.match.params;
     fetchModel(`/user/${userId}`)
       .then(({ data }) => this.setState({ user: data, error: null }))
       .catch((err) => this.setState({ user: null, error: err }));
-  }
+  };
 
   render() {
     const user = this.state.user;
-    if(!user) {
-      return (
-        <Typography variant="body1">
-          Loading user details...
-        </Typography>
-      );
+    if (!user) {
+      return <Typography variant="body1">Loading user details...</Typography>;
     }
     return (
-      <Typography variant="body1" className="user-detail">
-        <h2> {user.first_name} {user.last_name} </h2>
-        
-        <p> <b>Location:</b> {user.location} </p>
-        
-        <p> <b>Occupation:</b> {user.occupation} </p>
-        
-        <p> <b>Description:</b> {user.description }</p>
+      <Typography variant="body1" component="div" className="user-detail">
+        <h2>{user.first_name} {user.last_name}</h2>
+
+        <p><b>Location:</b> {user.location}</p>
+        <p><b>Occupation:</b> {user.occupation}</p>
+        <p><b>Description:</b> {user.description}</p>
         <p>
-          <a href={`#/photos/${user._id}`}>
+          <Link to={`/photos/${user._id}`}>
             View Photos of {user.first_name}
-          </a>
+          </Link>
         </p>
       </Typography>
     );
--- components/userPhotos/userPhotos.jsx
+++ components/userPhotos/userPhotos.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
-import {
-  Typography
-} from '@mui/material';
+import { Typography } from '@mui/material';
 import './userPhotos.css';
 import fetchModel from '../../lib/fetchModelData';
 
 /**
- * Define UserPhotos, a React componment of project #5
+ * Define UserPhotos, a React component of project #5
  */
+
 class UserPhotos extends React.Component {
   constructor(props) {
     super(props);
-
+    this.state = { photos: [], error: null };
   }
 
   componentDidMount() {
@@ -21,19 +20,49 @@ class UserPhotos extends React.Component {
       .catch((err) => this.setState({ photos: [], error: err }));
   }
 
+  componentDidUpdate(prevProps) {
+    if (prevProps.match.params.userId !== this.props.match.params.userId) {
+      const { userId } = this.props.match.params;
+      fetchModel(`/photosOfUser/${userId}`)
+        .then(({ data }) => this.setState({ photos: data || [], error: null }))
+        .catch((err) => this.setState({ photos: [], error: err }));
+    }
+  }
+
   render() {
     return (
-      <Typography variant="body1">
-      This should be the UserPhotos view of the PhotoShare app. Since
-      it is invoked from React Router the params from the route will be
-      in property match. So this should show details of user:
-      {this.props.match.params.userId}. You can fetch the model for the user from
-      window.models.photoOfUserModel(userId):
-        <Typography variant="caption">
-          {JSON.stringify(window.models.photoOfUserModel(this.props.match.params.userId))}
+      <>
+        <Typography variant="body1" component="div">
+          This should be the UserPhotos view of the PhotoShare app. Since
+          it is invoked from React Router the params from the route will be
+          in property match. So this should show details of user:
+          {this.props.match.params.userId}. You can fetch the model for the user from
+          window.models.photoOfUserModel(userId):
+          <Typography
+            variant="caption"
+            component="pre"
+            className="dev-json"
+            aria-hidden="true"
+          >
+            {JSON.stringify(window.models.photoOfUserModel(this.props.match.params.userId), null, 2)}
+          </Typography>
         </Typography>
-      </Typography>
 
+        <div className="user-photos-container">
+          {this.state.photos.map((p) => (
+            <div key={p._id} className="photo-card">
+              <img
+                className="photo-img"
+                src={`/images/${p.file_name}`}
+                alt={p.file_name}
+              />
+              <Typography variant="caption" display="block">
+                {p.date_time}
+              </Typography>
+            </div>
+          ))}
+        </div>
+      </>
     );
   }
 }
